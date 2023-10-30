aa1bd1ef9139ce1a5b9a0e7be0405781b4712fb4
Ryan Fernandes <ryan@fernandes.us>
2023-10-29 20:59:42 -0400

    Update arduino_code.ino

--- arduino/arduino_code.ino
+++ arduino/arduino_code.ino
@@ -5,14 +5,14 @@ Servo servo1;
 int servoPin = 13;
 int rotation = 0;
 
-
+//starts serial connection for communication with api program and attaches pin for servo
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(1);
   servo1.attach(servoPin);
 }
 
-
+//waits for serial communication and then moves servo to speicified point; used arduino documentation as reference
 void loop() {
   while (!Serial.available());
   rotation = Serial.readString().toInt();
